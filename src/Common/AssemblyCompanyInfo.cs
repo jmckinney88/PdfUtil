3189075e59ff166112693a40f1832667792c03d9
Jonathan McKinney <jmlonghorn88@gmail.com>
2016-01-13 21:09:40 -0600

    Initial Project Structure

---
+++ src/Common/AssemblyCompanyInfo.cs
@@ -0,0 +1,8 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+
+[assembly: AssemblyCompany("Jonathan McKinney")]
+[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyTrademark("")]
